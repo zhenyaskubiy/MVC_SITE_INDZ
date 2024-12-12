bc9b5c6ee9fd9c3c7422d9dcc7fb4d2b8e7da8b0
Yevgniya Skubiy <yevgeniyaskubiy@gmail.com>
2024-12-12 23:22:38 +0200

    додано для зв'язування ef і бд

---
+++ MVC_SITE_INDZ/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_SITE_INDZ.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
