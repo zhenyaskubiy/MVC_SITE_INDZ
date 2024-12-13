0db07163903ca245f32dad1ce0a0912e29781126
Ururu <0668090426fed@gmail.com>
2024-12-13 21:41:12 +0200

    додано модуль реєстрації

---
+++ MVC_SITE_INDZ/Models/User.cs
@@ -0,0 +1,9 @@
+namespace MVC_SITE_INDZ.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
