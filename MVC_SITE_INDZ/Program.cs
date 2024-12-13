6f0a5f5a847b3745c111ee4d31635cc599cc0e38
Yevgniya Skubiy <yevgeniyaskubiy@gmail.com>
2024-12-13 22:10:06 +0200

    додано підключення в файл

--- MVC_SITE_INDZ/Program.cs
+++ MVC_SITE_INDZ/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 
