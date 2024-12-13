5cc287e0a4370a91fe2a38af1d305ae3b61a8d66
Ururu <0668090426fed@gmail.com>
2024-12-13 22:01:30 +0200

    додано модуль для входу користувача

--- MVC_SITE_INDZ/Controllers/AccountController.cs
+++ MVC_SITE_INDZ/Controllers/AccountController.cs
@@ -14,6 +14,34 @@ namespace MVC_SITE_INDZ.Controllers
             _context = context;
         }
 
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user != null)
+            {
+                HttpContext.Session.SetString("Username", user.Username);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ErrorMessage = "Невірний логін або пароль.";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("Username");
+            return RedirectToAction("Login", "Account");
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
--- MVC_SITE_INDZ/Program.cs
+++ MVC_SITE_INDZ/Program.cs
@@ -3,16 +3,24 @@ using MVC_SITE_INDZ.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
